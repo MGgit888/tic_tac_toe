c0a67a6c892ae5c62ba917e98d775400817f8640
MGgit888 <77639566+MGgit888@users.noreply.github.com>
2024-11-23 10:46:52 -0500

    Re-added docstrings to logic python

--- tic_tac_toe_logic.py
+++ tic_tac_toe_logic.py
@@ -3,14 +3,18 @@ import random
 from typing import List, Dict
 
 class TicTacToeGame:
+    """Represents a Tic Tac Toe game."""
     def __init__(self):
+        """Initializes a new game of Tic Tac Toe."""
         self.reset_board()
 
     def reset_board(self):
+        """Resets the game board and sets the current player to X."""
         self.current_board: List[str] = [str(i) for i in range(1, 10)]
         self.current_player = "X"
 
     def get_board_json(self) -> Dict[str, str]:
+        """Returns the current state of the game board as a JSON dictionary."""
         return {
             "row1": "".join(self.current_board[0:3]),
             "row2": "".join(self.current_board[3:6]),
@@ -18,12 +22,22 @@ class TicTacToeGame:
         }
 
     def make_move(self, index: int) -> Dict[str, str]:
+        """Makes a move on the game board at the given index.
+
+        Args:
+            index: The index of the cell to play on (1-9).
+
+        Returns:
+            A dictionary containing the updated game state or an error message
+            if the move is invalid.
+        """
         idx = index - 1
         if idx < 0 or idx >= len(self.current_board) or self.current_board[idx] in ["X", "O"]:
             return {"error": "Invalid move"}
 
         self.current_board[idx] = self.current_player
 
+        # Check if there is a winner or a tie
         if self.check_winner(self.current_player):
             response = {
                 "message": f"{self.current_player} wins!",
@@ -43,6 +57,7 @@ class TicTacToeGame:
         self.switch_player()
         self.computer_move()
 
+        # Check if there is a winner or tie after the computer move
         if self.check_winner(self.current_player):
             response = {
                 "message": f"{self.current_player} wins!",
@@ -63,15 +78,25 @@ class TicTacToeGame:
         return self.get_board_json()
 
     def switch_player(self):
+        """Switches the current player."""
         self.current_player = "O" if self.current_player == "X" else "X"
 
     def computer_move(self):
+        """Picks a random available place to make the computer move."""
         empty_indices = [i for i, x in enumerate(self.current_board) if x not in ["X", "O"]]
         if empty_indices:
             comp_idx = random.choice(empty_indices)
             self.current_board[comp_idx] = self.current_player
 
     def check_winner(self, player: str) -> bool:
+        """Checks if the given player has a winning combination on the board.
+
+        Args:
+            player: The player to check for a win.
+
+        Returns:
+            True if the player has a winning combination, False otherwise.
+        """
         win_conditions = [
             (0, 1, 2),
             (3, 4, 5),
